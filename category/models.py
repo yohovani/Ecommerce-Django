9db3c23b31a8d49aa429917fd309e78f164b3f68
yohovani <yohovanivargas@gmail.com>
2022-02-01 13:58:50 -0600

    first commit

---
+++ category/models.py
@@ -0,0 +1,20 @@
+from distutils.command.upload import upload
+from pyexpat import model
+from tabnanny import verbose
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50, unique=True)
+    description = models.CharField(max_length=255, blank = True)
+    slug = models.CharField(max_length=100, unique=True)
+    cat_image = models.ImageField(upload_to = 'photos/categories', blank = True)
+
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+        
+    def __str__(self):
+        return self.category_name
