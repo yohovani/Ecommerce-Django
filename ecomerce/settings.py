6ced5c5b02a84caa15cae0d7ac2824e267bd133d
yohovani <yohovanivargas@gmail.com>
2022-02-01 16:05:16 -0600

    Modelo productos y visualización en el dashboard

--- ecomerce/settings.py
+++ ecomerce/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'category',
-    'accounts'
+    'accounts',
+    'store',
 ]
 
 MIDDLEWARE = [
@@ -124,6 +125,10 @@ STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS = [
     'ecomerce/static'
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
