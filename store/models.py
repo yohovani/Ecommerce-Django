bcad81677b98e12d730712b0912c1a843fd015b7
yohovani <yohovanivargas@gmail.com>
2022-02-02 15:02:19 -0600

    Detalle Producto

--- store/models.py
+++ store/models.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 from distutils.command.upload import upload
 from pyexpat import model
 from django.db import models
@@ -15,6 +16,9 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
+
+    def get_url(self):
+        return reverse('product_detail', args=[self.category.slug, self.slug])
 
     def __str__(self):
         return self.product_name
