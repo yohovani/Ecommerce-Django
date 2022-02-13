98954e75afeefe2687385e3126986d2958c914ee
yohovani <yohovanivargas@gmail.com>
2022-02-13 12:30:53 -0600

    Diseño variaciones Ecommerce

--- store/models.py
+++ store/models.py
@@ -1,3 +1,4 @@
+from random import choice
 from django.urls import reverse
 from distutils.command.upload import upload
 from pyexpat import model
@@ -22,3 +23,27 @@ class Product(models.Model):
 
     def __str__(self):
         return self.product_name
+
+class VariationManager(models.Manager):
+    def colors(self):
+        return super(VariationManager, self).filter(variation_category='color', is_active=True)
+
+    def tallas(self):
+        return super(VariationManager, self).filter(variation_category='talla', is_active=True)
+
+variation_category_choice = (
+    ('color', 'color'),
+    ('talla', 'talla'),
+)
+
+class Variation(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category = models.CharField(max_length=100, choices=variation_category_choice)
+    variation_value = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now=True)
+
+    objects = VariationManager()
+
+    def __str__(self):
+        return self.variation_category + ':' + self.variation_value
